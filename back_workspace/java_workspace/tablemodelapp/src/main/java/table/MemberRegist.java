a261da30a5deb5e7f2479b5df3f6521ee6ea3d15
yejeeni <320418@naver.com>
2025-05-28 17:52:25 +0900

    day0528

--- back_workspace/java_workspace/tablemodelapp/src/main/java/table/MemberRegist.java
+++ back_workspace/java_workspace/tablemodelapp/src/main/java/table/MemberRegist.java
@@ -5,17 +5,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.RowId;
 import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -23,69 +26,128 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.TableModel;
 
-public class MemberRegist extends JFrame implements ActionListener, WindowListener{
-	// 서쪽 영역
+
+public class MemberRegist extends JFrame implements ActionListener, WindowListener, MouseListener{
+	//서쪽 영역 
 	JPanel p_west;
 	JTextField t_id;
 	JTextField t_name;
 	JTextField t_tel;
 	JButton bt;
 	
-	// 센터 영역
+	//센터영역
 	JPanel p_center;
 	JTable table;
 	JScrollPane scroll;
 	
-	TableModel model;
+	//센터 하단
+	JPanel p_south;
+	JLabel la_id;
+	JLabel la_value;
+	JLabel la_name;
+	JLabel la_tel;
+	JTextField t_name2;
+	JTextField t_tel2;
+	JButton bt_edit;
+	JButton bt_del;
 	
+	TableModel model;
+	int index=0;
+	int selectId = 0; // 현재 선택한 데이터의 pk id 
+	String[] selectMember = null;
 	
 	Connection connection; // 접속은 윈도우 창 생성 시 한 번 시도되며, 창을 닫을 때 접속을 해제
 	
 	public MemberRegist() {
+		//생성
 		p_west = new JPanel();
 		t_id = new JTextField();
-		t_name = new JTextField();
+		t_name=new JTextField();
 		t_tel = new JTextField();
+		
+		// 이미지 적용. 이미지의 경로는 class path를 기준으로 가져오기
+		Class myClass = this.getClass();
+		URL url = myClass.getClassLoader().getSystemResource("handshake.png");
+		
 		bt = new JButton("가입");
 		
 		p_center = new JPanel();
-				
-		// model을 부모인 TableModel로 정의해놓고, 사용할 테이블의 자식 클래스로 다운캐스팅
-		model = new MyModel();
-		table = new JTable(model); // 생성자의 인수에 이 테이블에서 보여줘야 할 데이터 또는 데이터 처리 객체가 들어가야 함 (MVC패턴에서 모델+컨트롤러의 느낌). 실제 보여질 데이터는 모델이 결정
+
+		table = new JTable(model =new MyModel(this));  // 생성자의 인수에 이 테이블에서 보여줘야 할 데이터 또는 데이터 처리 객체가 들어가야 함 (MVC패턴에서 모델+컨트롤러의 느낌). 실제 보여질 데이터는 모델이 결정
 		scroll = new JScrollPane(table);
+		
+		//새로 추가된 센터 영역 컴포넌트 
+		p_south = new JPanel();
+		la_id = new JLabel("ID");
+		la_name = new JLabel("Name");
+		la_tel = new JLabel("Tel");
+		la_value=new JLabel("");
+		
+		t_name2 = new JTextField();
+		t_tel2 = new JTextField();
+		bt_edit = new JButton("수정");
+		bt_del = new JButton("삭제");
+		
 		
 		// 스타일 적용
 		p_west.setBackground(Color.ORANGE);
 		p_west.setPreferredSize(new Dimension(150, 500));
-		
-		Dimension dimension = new Dimension(146, 35);
-		t_id.setPreferredSize(dimension);
-		t_name.setPreferredSize(dimension);
-		t_tel.setPreferredSize(dimension);
 
-		scroll.setPreferredSize(new Dimension(430, 450));
+		Dimension d = new Dimension(146, 35);
+		t_id.setPreferredSize(d);
+		t_name.setPreferredSize(d);
+		t_tel.setPreferredSize(d);
 		
-		bt.addActionListener(this);
+		scroll.setPreferredSize(new Dimension(435, 350));
 		
-		// 윈도우 창과 리스너와의 연결
-		addWindowListener(this);
+		p_south.setPreferredSize(new Dimension(450, 145));
+		p_south.setBackground(Color.YELLOW);
 		
-		// 조립
+		Dimension d2 = new Dimension(200, 30);		
+		la_id.setPreferredSize(d2);
+		la_name.setPreferredSize(d2);
+		la_tel.setPreferredSize(d2);
+		
+		Dimension d3 = new Dimension(370, 30);
+		la_value.setPreferredSize(d3);
+		t_name2.setPreferredSize(d3);
+		t_tel2.setPreferredSize(d3);
+		
+		
+		//조립 
 		p_west.add(t_id);
 		p_west.add(t_name);
 		p_west.add(t_tel);
 		p_west.add(bt);
-		add(p_west, BorderLayout.WEST); 
-
-
+		add(p_west, BorderLayout.WEST);
+	
 		p_center.add(scroll);
 		add(p_center);
+		add(p_south, BorderLayout.SOUTH);
 		
-		setBounds(300, 100, 600, 500);
+		//남쪽 패널에 부착 
+		p_south.add(la_id);
+		p_south.add(la_value);
+		p_south.add(la_name);
+		p_south.add(t_name2);
+		p_south.add(la_tel);
+		p_south.add(t_tel2);
+		p_south.add(bt_edit);
+		p_south.add(bt_del);
+		
+		
+		bt.addActionListener(this); //버튼과 리스너 연결 
+		table.addMouseListener(this); // 테이블과 리스너 연결
+		bt_del.addActionListener(this); // 삭제 버튼과 리스너 연결
+		bt_edit.addActionListener(this); // 수정 버튼과 리스너 연결
+		
+		//윈도우창과 리스너와의 연결 
+		this.addWindowListener(this);
+		
+		setBounds(300,100, 600, 500);
 		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+		//setDefaultCloseOperation(EXIT_ON_CLOSE);
+		 
 		// 데이터베이스 접속
 		connect();
 		selectAll(); // 전체 회원 조회
@@ -94,17 +156,14 @@ public class MemberRegist extends JFrame implements ActionListener, WindowListen
 	public void connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/dev";
-			String id = "java";
-			String pwd = "1234";
+			String url="jdbc:mysql://localhost:3306/dev";
+			String id="java";
+			String pwd="1234";
 			
-			connection = DriverManager.getConnection(url, id, pwd);
-			if (connection != null) {
-				// 윈도우창 제목에 "접속 성공"을 출력
+			connection=DriverManager.getConnection(url, id, pwd);
+			if(connection!=null) {
 				this.setTitle("접속 성공");
-				System.out.println("데이터베이스 접속 성공");
 			}
-			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -150,7 +209,9 @@ public class MemberRegist extends JFrame implements ActionListener, WindowListen
 //		table.updateUI();
 	}
 	
-	// 데이터 가져오기
+	/**
+	 * 모든 회원 정보 가져오기
+	 */
 	public void selectAll() {
 		String sql = "select * from member4";
 		PreparedStatement preparedStatement = null;
@@ -165,7 +226,7 @@ public class MemberRegist extends JFrame implements ActionListener, WindowListen
 			
 			// resultSet 자체는 Model이 보유하고 있는 2차원 배열 자체가 아니므로, resultSet의 데이터를 2차원 배열로 변환하여 Model에서 배열 대신 사용해야 함
 			// total의 값으로 2차원 배열 재정의
-			((MyModel)model).rows = new String[total][3];
+			((MyModel)model).rows = new String[total][4];
 			
 			// 레코드의 마지막으로 이동했던 커서를 맨 앞으로 다시 복귀
 			resultSet.beforeFirst();
@@ -176,7 +237,8 @@ public class MemberRegist extends JFrame implements ActionListener, WindowListen
 				String[] record = {
 						resultSet.getString("id"), 
 						resultSet.getString("name"),  
-						resultSet.getString("tel")
+						resultSet.getString("tel"),
+						resultSet.getString("member4_id"),
 						};
 				
 				((MyModel)model).rows[index++] = record;
@@ -203,8 +265,126 @@ public class MemberRegist extends JFrame implements ActionListener, WindowListen
 		}
 	}
 	
+	/**
+	 * 선택한 회원 정보 가져오기
+	 */
+	public void select(int member4_id) {
+//		System.out.println("사원 선택");
+		String sql = "select * from member4 where member4_id = " + member4_id;
+		System.out.println(sql);
+		
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery(); // 레코드 결과 반환
+			
+			// 화면에 출력
+			if(resultSet.next()) { // 레코드가 존재하는 경우
+				String id = resultSet.getString("id");
+				this.la_value.setText(id);
+				
+				String name = resultSet.getString("name");
+				this.t_name2.setText(name);
+				
+				String tel = resultSet.getString("tel");
+				this.t_tel2.setText(tel);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(resultSet != null) {
+					try {
+						resultSet.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				
+			}
+		}
+	}
+	
+	/**
+	 * 선택한 회원 1명 삭제
+	 */
+	public void delete(int member4_id) {
+		String sql = "delete from member4 where member4_id = " + member4_id;
+//		System.out.println(sql);
+		
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement(sql); // 쿼리 객체
+			int result = preparedStatement.executeUpdate(); // DML 수행. 쿼리가 수행된 레코드 수를 반환한다. 반한값이 0인 경우는 실행이 되지 않은 것
+			if(result > 0) {
+				JOptionPane.showMessageDialog(this, "삭제 성공");
+				selectAll();
+			} else {
+				JOptionPane.showMessageDialog(this, "삭제에 실패했습니다.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (preparedStatement != null) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}	
+	}
+	
+	/**
+	 * 선택한 회원 1명의 정보 수정
+	 * @param record 회원 1명의 정보 배열
+	 */
+	public void edit(String[] record) {
+		String sql = "update member4 set id= '" + record[0] + "', name= '"+ record[1]+ "', tel='" + record[2] + "'";
+		sql += " where member4_id= " + record[3];
+		System.out.println(sql);
+		
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			int result = preparedStatement.executeUpdate();
+			if(result > 0) {
+				JOptionPane.showMessageDialog(this, "수정되었습니다.");
+				selectAll();
+			} else {
+				JOptionPane.showConfirmDialog(this, "수정에 실패했습니다.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(preparedStatement != null) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
 	public void actionPerformed(ActionEvent e) {
-		regist();
+		Object object = e.getSource();
+		if(object == bt) {
+			regist();			
+		}else if(object == bt_edit) {
+			int result = JOptionPane.showConfirmDialog(this, "수정하시겠습니까?");
+			if (result == JOptionPane.OK_OPTION) {
+				selectMember[1] = t_name2.getText();
+				selectMember[2] = t_tel2.getText();
+				edit(selectMember);
+			}
+		}else if(object == bt_del) {
+			int result = JOptionPane.showConfirmDialog(this, "삭제하시겠습니까?");
+			if (result == JOptionPane.OK_OPTION) {
+				delete(selectId);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -250,6 +430,43 @@ public class MemberRegist extends JFrame implements ActionListener, WindowListen
 	@Override
 	public void windowDeactivated(WindowEvent e) {
 //		System.out.println("windowDeactivated");
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		int row = table.getSelectedRow(); // 선택한 행의 값
+		int col = table.getSelectedColumn(); // 선택한 열의 값
+//		// 행, 열 값을 통해 데이터 베열에 접근해서 값을 가져오는 방식
+		// JTable에 출력하지 않은 숨겨진 데이터도 접근 가능
+		System.out.println(((MyModel)model).rows[row][3]);
+		
+		String[][] rows = ((MyModel)model).rows;
+		
+		// JTable에서 자신의 셀의 정보를 반환하는 것을 통해 값을 가져오는 방식
+//		Object value = table.getValueAt(row, 1);
+//		System.out.println(value);
+		
+		selectId = Integer.parseInt(((MyModel)model).rows[row][3]);
+		selectMember = rows[row];
+		select(Integer.parseInt(((MyModel)model).rows[row][3]));
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
 	}
 
 	
--- back_workspace/java_workspace/tablemodelapp/src/main/java/table/MyModel.java
+++ back_workspace/java_workspace/tablemodelapp/src/main/java/table/MyModel.java
@@ -1,18 +1,33 @@
 package table;
 
+import java.awt.Window;
+
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
 
 /**
  * JTable은 MVC 패턴과 비슷한 컴포넌트인데, 데이터를 보유하고, 그 데이터를 디자인 영역에 반영하는 코드가 포함되어있어 MC의 역할을 동시에 수행하는 것과 비슷함
  * 중요한 것은 JTable과 데이터를 분리시켜놓았다는 점
+ * TableModelListener: 모델이 보유한 데이터를 수정할 때 발생되는 이벤트를 감지하는 리스너
  */
-public class MyModel extends AbstractTableModel {
+public class MyModel extends AbstractTableModel implements TableModelListener{
 
 	// 회원 정보 2차원 배열
-	String[][] rows = new String[0][3];
+	String[][] rows = new String[0][4];
 	String[] columns = {"ID", "NAME", "TEL"};
+	MemberRegist memberRegist;
 	
+	// 생성자
+	public MyModel(MemberRegist memberRegist) {
+		
+		this.memberRegist = memberRegist;
+		
+		// 모델과 리스너 연결
+		this.addTableModelListener(this);
+	}
 	
+		
 	/**
 	 * 테이블의 총 레코드 수
 	 */
@@ -46,13 +61,40 @@ public class MyModel extends AbstractTableModel {
 		return rows[rowIndex][columnIndex];
 	}
 	
+	// 테이블 셀에서 데이터를 편집해도, 현재 모델이 보유한 2차원배열을 수정하지 않는 한 값 수정 반영이 되지 않아, setter가 필요함
+	/**
+	 * 셀에서 원하는 데이터를 입력하고 엔터를 치는 순간, 해당 셀의 row, col, value가 전달
+	 */
+	@Override
+	public void setValueAt(Object value, int rowIndex, int columnIndex) {
+//		System.out.println("setValueAt() " + value + rowIndex + columnIndex);
+		
+		// 모델의 2차원 배열에 값을 반영
+		rows[rowIndex][columnIndex] = (String) value;
+		// memberRegist의 edit() 메서드 호출하여 데이터베이스에 반영
+		this.memberRegist.edit(rows[rowIndex]);
+		
+//		super.setValueAt(value, rowIndex, columnIndex);
+	}
+	
 	/**
 	 * 셀을 수정할 수 있는지
 	 */
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		System.out.println(rowIndex+"행, "+columnIndex+"열은 수정 가능");
-		return true;
+		if (columnIndex > 0) {
+			return true;			
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 테이블의 셀을 수정 후 엔터를 누르면 호출되는 메서드
+	 */
+	@Override
+	public void tableChanged(TableModelEvent e) {
+		System.out.println("tableChanged()");
 	}
 	
 }
