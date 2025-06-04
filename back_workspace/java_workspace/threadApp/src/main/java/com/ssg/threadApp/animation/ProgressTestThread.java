ada5472976f1e1c888160182dc020151a3f0c052
yejeeni <320418@naver.com>
2025-06-05 08:54:05 +0900

    ProgressTestThread 수정

--- back_workspace/java_workspace/threadApp/src/main/java/com/ssg/threadApp/animation/ProgressTestThread.java
+++ back_workspace/java_workspace/threadApp/src/main/java/com/ssg/threadApp/animation/ProgressTestThread.java
@@ -4,31 +4,23 @@ import javax.swing.JProgressBar;
 
 public class ProgressTestThread {
 
-	
 	public Thread returnThread(JProgressBar bar, int sleep) {
-		int n = 2;
-		
 		Thread thread = new Thread() {
+			int n = 0;
+
 			@Override
 			public void run() {
-				while(true) {
+				while (true) {
 					try {
 						Thread.sleep(sleep);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					move(bar, n);
+					n += 2;
+					bar.setValue(n);
 				}
 			}
 		};
-		
 		return thread;
 	}
-	
-
-	public void move(JProgressBar bar, int n) {
-		n+=2;
-		bar.setValue(n); // bar 증가
-	}
 }
-
